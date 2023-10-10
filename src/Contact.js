e8223e0ef61afda09b2ee240774f354bb3e05ea7
chinhbean09 <chinh0726@gmail.com>
2023-10-10 23:51:26 +0700

    Complete React Router Dom v6 & Create App Pages

---
+++ src/Contact.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Contact = () => {
+  return (
+    <Wrapper>
+      <h1>Contact</h1>
+    </Wrapper>
+  );
+  };
+  const Wrapper = styled.section`
+  
+  `;
+  
+
+
+export default Contact;
